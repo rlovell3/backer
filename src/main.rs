ab568045153f426359c0289d0a31c191d99632c7
Ross Lovell <admin@gridvision.net>
2023-12-09 12:08:18 -0500

    Switched to io::copy due to fs issue on Windows OS.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
-use std::fs;
+//use std::fs;
+use std::fs::File;
+use std::io::copy;
 use std::path::{Path, PathBuf};
 use std::process;
 
@@ -28,10 +30,34 @@ fn main() {
     let formatted_datetime = current_datetime.format("%Y%m%d_%H%M").to_string();
     let backup_filename = format!("{}.{}.backup", file_path.file_stem().unwrap().to_str().unwrap(), formatted_datetime);
 
-    let backup_path = file_path.with_file_name(&backup_filename);
+    /* let backup_path = file_path.with_file_name(&backup_filename);
     println!("Backup path: {}", backup_path.display());
     
     match fs::copy(&file_path, &backup_path) {
+        Ok(_) => println!("Backup successful: {}", backup_path.display()),
+        Err(e) => eprintln!("Error during backup: {}", e),
+    } */
+
+    let backup_path = file_path.with_file_name(&backup_filename);
+    println!("Backup path: {}", backup_path.display());
+
+    let mut source_file = match File::open(&file_path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error opening source file: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let mut dest_file = match File::create(&backup_path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error creating backup file: {}", e);
+            process::exit(1);
+        }
+    };
+
+    match copy(&mut source_file, &mut dest_file) {
         Ok(_) => println!("Backup successful: {}", backup_path.display()),
         Err(e) => eprintln!("Error during backup: {}", e),
     }
